0a5f9a0e372eec1c82ab1ac7bf4e0468e919634c
luxuze <luxuze@icloud.com>
2020-03-26 18:13:02 +0800

    add test case

--- examples/example.go
+++ examples/example.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	client := request.Client{
-		URL:    "https://baidu.com",
+		URL:    "https://google.com",
 		Method: "POST",
 		Params: map[string]string{"hello": "world"},
 		Body:   []byte(`{"hello": "world"}`),
