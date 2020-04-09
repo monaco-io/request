136f052d6dd0ffa2c069ba4dbabdc5777167c15d
luxuze <luxuze@icloud.com>
2020-04-09 14:17:46 +0800

    Update request_test.go

--- request_test.go
+++ request_test.go
@@ -279,6 +279,15 @@ func TestClient_Resp(t *testing.T) {
 			wantResp: nil,
 			wantErr:  true,
 		},
+		{
+			name: "TestClient_Resp_build_error",
+			fields: fields{
+				URL:    " ",
+				Method: "POST",
+			},
+			wantResp: nil,
+			wantErr:  true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -401,6 +410,40 @@ func TestClient_StatusCode(t *testing.T) {
 			resp, _ := c.Do()
 			if gotCode := resp.StatusCode(); gotCode != tt.wantCode {
 				t.Errorf("StatusCode() = %v, want %v", gotCode, tt.wantCode)
+			}
+		})
+	}
+}
+
+func TestSugaredResp_Status(t *testing.T) {
+	type fields struct {
+		Data []byte
+		Code int
+		resp *http.Response
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		wantStatus string
+	}{
+		{
+			name: "TestSugaredResp_Status_200_ok",
+			fields: fields{
+				Data: nil,
+				Code: 0,
+				resp: nil,
+			},
+			wantStatus: "200 OK",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Client{
+				URL: serverURL + "/get",
+			}
+			s, _ := c.Do()
+			if gotStatus := s.Status(); gotStatus != tt.wantStatus {
+				t.Errorf("Status() = %v, want %v", gotStatus, tt.wantStatus)
 			}
 		})
 	}
