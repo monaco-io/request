fc0b402e6815673f114e380f4937ca80531a5401
luxuze <luxuze@icloud.com>
2020-06-07 23:34:00 +0800

    refactor: build client

---
+++ build.go
@@ -0,0 +1,88 @@
+package request
+
+import (
+	"bytes"
+	"crypto/tls"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"time"
+)
+
+func (c *Client) buildRequest() (err error) {
+	if err = c.applyRequest(); err != nil {
+		return
+	}
+	c.applyHTTPHeader()
+	c.applyBasicAuth()
+	c.applyClient()
+	c.applyTimeout()
+	c.applyCookies()
+	c.applyProxy()
+	return
+}
+
+func (c *Client) applyRequest() (err error) {
+	// encode requestURL.httpURL like https://google.com?hello=world&package=request
+	c.requestURL = requestURL{
+		urlString:  c.URL,
+		parameters: c.Params,
+	}
+	if err = c.requestURL.EncodeURL(); err != nil {
+		return
+	}
+	c.req, err = http.NewRequest(c.Method, c.requestURL.string(), bytes.NewReader(c.Body))
+	return
+}
+
+func (c *Client) applyHTTPHeader() {
+	if c.Method == POST {
+		if c.ContentType == emptyString {
+			c.ContentType = ApplicationJSON
+		}
+		c.req.Header.Set(contentType, string(c.ContentType))
+	}
+	for k, v := range c.Header {
+		c.req.Header.Add(k, v)
+	}
+}
+
+func (c *Client) applyBasicAuth() {
+	if c.BasicAuth.Username != emptyString && c.BasicAuth.Password != emptyString {
+		c.req.SetBasicAuth(c.BasicAuth.Username, c.BasicAuth.Password)
+	}
+}
+
+func (c *Client) applyClient() {
+	c.client = &http.Client{}
+}
+
+func (c *Client) applyTimeout() {
+	if c.Timeout > 0 {
+		c.client.Timeout = c.Timeout * time.Second
+	}
+}
+
+func (c *Client) applyCookies() {
+	if c.Cookies != nil {
+		jar, _ := cookiejar.New(nil)
+		jar.SetCookies(&url.URL{Scheme: c.requestURL.scheme(), Host: c.requestURL.host()}, c.Cookies)
+		c.client.Jar = jar
+	}
+}
+
+// TODO: test case
+func (c *Client) applyProxy() (err error) {
+	if c.ProxyURL != emptyString {
+		var proxy *url.URL
+		if proxy, err = url.Parse(c.ProxyURL); err != nil {
+			return
+		} else if proxy != nil {
+			c.client.Transport = &http.Transport{
+				Proxy:           http.ProxyURL(proxy),
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+		}
+	}
+	return
+}
--- request.go
+++ request.go
@@ -1,13 +1,8 @@
 package request
 
 import (
-	"bytes"
-	"crypto/tls"
 	"io/ioutil"
 	"net/http"
-	"net/http/cookiejar"
-	"net/url"
-	"time"
 )
 
 // Do send http request
@@ -27,80 +22,6 @@ func (c *Client) Do() (resp SugaredResp, err error) {
 	resp.Data, err = ioutil.ReadAll(resp.resp.Body)
 	resp.Code = resp.resp.StatusCode
 	return
-}
-
-func (c *Client) buildRequest() (err error) {
-	if err = c.applyRequest(); err != nil {
-		return
-	}
-	c.applyHTTPMethod()
-	c.applyBasicAuth()
-	c.applyClient()
-	c.applyTimeout()
-	c.applyCookies()
-	c.applyProxy()
-	return
-}
-
-func (c *Client) applyRequest() (err error) {
-	// encode requestURL.httpURL like https://google.com?hello=world&package=request
-	c.requestURL = requestURL{
-		urlString:  c.URL,
-		parameters: c.Params,
-	}
-	if err = c.requestURL.EncodeURL(); err != nil {
-		return
-	}
-	c.req, err = http.NewRequest(c.Method, c.requestURL.string(), bytes.NewReader(c.Body))
-	return
-}
-
-func (c *Client) applyHTTPMethod() {
-	if c.Method == POST {
-		if c.ContentType == emptyString {
-			c.ContentType = ApplicationJSON
-		}
-		c.req.Header.Set(contentType, string(c.ContentType))
-	}
-	for k, v := range c.Header {
-		c.req.Header.Add(k, v)
-	}
-}
-
-func (c *Client) applyBasicAuth() {
-	if c.BasicAuth.Username != emptyString && c.BasicAuth.Password != emptyString {
-		c.req.SetBasicAuth(c.BasicAuth.Username, c.BasicAuth.Password)
-	}
-}
-
-func (c *Client) applyClient() {
-	c.client = &http.Client{}
-}
-
-func (c *Client) applyTimeout() {
-	if c.Timeout > 0 {
-		c.client.Timeout = c.Timeout * time.Second
-	}
-}
-
-func (c *Client) applyCookies() {
-	if c.Cookies != nil {
-		jar, _ := cookiejar.New(nil)
-		jar.SetCookies(&url.URL{Scheme: c.requestURL.scheme(), Host: c.requestURL.host()}, c.Cookies)
-		c.client.Jar = jar
-	}
-}
-
-// TODO: raise proxy error
-func (c *Client) applyProxy() {
-	if c.ProxyURL != emptyString {
-		if proxy, err := url.Parse(c.ProxyURL); err == nil && proxy != nil {
-			c.client.Transport = &http.Transport{
-				Proxy:           http.ProxyURL(proxy),
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			}
-		}
-	}
 }
 
 // Resp do request and get original http response struct
