f2433087d8aafbd5490248dda640bea84d138282
luxuze <luxuze@icloud.com>
2020-06-19 15:45:14 +0800

    new line before comments

--- const.go
+++ const.go
@@ -14,20 +14,28 @@ const (
 const (
 	// OPTIONS http options
 	OPTIONS = "OPTIONS"
+
 	// GET http get
 	GET = "GET"
+
 	// HEAD http head
 	HEAD = "HEAD"
+
 	// POST http post
 	POST = "POST"
+
 	// PUT http put
 	PUT = "PUT"
+
 	// DELETE http delete
 	DELETE = "DELETE"
+
 	// TRACE http trace
 	TRACE = "TRACE"
+
 	// CONNECT http connect
 	CONNECT = "CONNECT"
+
 	// PATCH http patch
 	PATCH = "PATCH"
 )
